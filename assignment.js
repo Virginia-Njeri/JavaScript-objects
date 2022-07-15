6bf421cfafe9de7c7be90bf91bbe0d3c8adf7f7f
virginia-njeri <you@example.comvirginiaanjeri@gmail.com>
2022-07-15 08:30:52 +0300

    first commit

---
+++ assignment.js
@@ -0,0 +1,21 @@
+
+
+var kiosk=[
+    {
+        name :"Orange",
+        price : 30,
+    },
+    {
+        name :"Mango",
+        price : 15,
+    },
+    {
+        name :"Avocado",
+        price : 40,
+    }]
+ function getTotalCost(name,quantity){
+    let a=kiosk.find(item=>item.name===name)
+    console.log(`${quantity} ${name} for KES ${a.price*quantity}`)
+     
+}
+getTotalCost("Avocado",3)
